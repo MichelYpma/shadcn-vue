0c612b6787ec6b555d449127d4de7768e7a3c0f8
MrYpma <michel@mrynk.nl>
2024-11-13 22:29:34 +0100

    max support on Progress
    Take the max property into consideration when updating the ProgressIndicator

--- apps/www/src/lib/registry/default/ui/progress/Progress.vue
+++ apps/www/src/lib/registry/default/ui/progress/Progress.vue
@@ -33,7 +33,7 @@ const delegatedProps = computed(() => {
   >
     <ProgressIndicator
       class="h-full w-full flex-1 bg-primary transition-all"
-      :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
+      :style="`transform: translateX(-${100 - (props.modelValue ?? 0) * 100 / (props.max ?? 100)}%);`"
     />
   </ProgressRoot>
 </template>
